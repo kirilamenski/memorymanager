0f082cdacb4e41ff14bb929e1fd58a656d3c1349
KirilaMenski <bv_1514@mail.ru>
2018-09-02 14:12:44 +0300

    add frame callback

--- app/src/main/java/com/ansgar/memorymanager/MemoryManagerService.kt
+++ app/src/main/java/com/ansgar/memorymanager/MemoryManagerService.kt
@@ -2,14 +2,20 @@ package com.ansgar.memorymanager
 
 import android.app.Service
 import android.content.Intent
+import android.os.Build
+import android.os.Handler
 import android.os.IBinder
+import android.util.Log
+import android.view.Choreographer
 import java.util.*
+import android.os.Looper
+
 
 internal class MemoryManagerService : Service() {
 
     private val tag = MemoryManagerService::class.java.canonicalName
 
-    var timer: Timer? = null
+    private var timer: Timer? = null
 
     override fun onCreate() {
         timer = Timer()
@@ -25,8 +31,19 @@ internal class MemoryManagerService : Service() {
                 responseIntent.action = Constants.EXTRA_ACTION
                 responseIntent.putExtra(Constants.EXTRA_MEMORY_USAGE_DATA, memory)
                 sendBroadcast(responseIntent)
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    val frameCallback = Choreographer.FrameCallback { nanos ->
+                        Log.i("!!!!", "Nanos: $nanos")
+                    }
+
+                    Handler(Looper.getMainLooper()).post {
+                        Choreographer.getInstance().postFrameCallback(frameCallback)
+                    }
+                }
+
             }
-        }, 5000, delay)
+        }, 2000, delay)
 
         return Service.START_STICKY
     }
